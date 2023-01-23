7e33746ad40ba887ee43be717f0c554024801a2d
Morgan <morgan.fromentin@gmail.com>
2023-01-23 10:20:44 +0100

    adding tests for AppController

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -6,7 +6,12 @@ import { AppService } from './app.service';
 describe('AppController', () => {
     let controller: AppController;
     const logger = winston.createLogger();
-    let appService;
+    let appService: AppService;
+
+    const response = {
+        status: () => null,
+        send: () => null
+    };
 
     beforeEach(() => {
         controller = new AppController(
@@ -25,6 +30,28 @@ describe('AppController', () => {
             const res = controller.helloWorld();
             return (expect(logger.info) as any).to.have.been.calledWith('Hello World!')
                 && (expect(res) as any).to.be.deep.eq('Hello World!');
+        });
+    });
+
+    describe('validation', () => {
+        it('should call appService.validate and response.status().send() with correct params if reasons found', () => {
+            stub(appService, 'validate').returns({result: 'ok', reasons: {some: 'reasons'}});
+            stub(response, 'status');
+            stub(response, 'send');
+            controller.validation({some: 'data'} as any, response);
+            return (expect(appService.validate) as any).to.have.been.calledWith({some: 'data'})
+                && (expect(response.status) as any).to.have.been.calledWith(418)
+                && (expect(response.send) as any).to.have.been.calledWith({statusCode: 418, reasons: {some: 'reasons'}});
+        });
+
+        it('should call appService.validate and response.status().send() with correct params if no reasons found', () => {
+            stub(appService, 'validate').returns({result: 'ok', reasons: null});
+            stub(response, 'status');
+            stub(response, 'send');
+            controller.validation({some: 'data'} as any, response);
+            return (expect(appService.validate) as any).to.have.been.calledWith({some: 'data'})
+                && (expect(response.status) as any).to.have.been.calledWith(202)
+                && (expect(response.send) as any).to.have.been.calledWith({statusCode: 202, message: 'Accepted'});
         });
     });
 
